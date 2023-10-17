62d78b4b02ffa80319770c0bd87d9e293d717fb3
CarlosHerazo <102492748+CarlosHerazo@users.noreply.github.com>
2023-10-16 22:04:40 -0500

    Inicio de sesion para el Admin realizado

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <?php
     require './controllers/config.php';
+    require './models/database.php';
 
+ // Establecer una conexión a la base de datos
+$db = new Database();
+$con = $db->conectar();
+
+if ($_POST) {
+    $usuario = $_POST['usuario'];
+    $contra = $_POST['contra'];
+
+    // Consulta SQL para verificar las credenciales del usuario
+    $sql = "SELECT id, password, nombre FROM admin WHERE user = :usuario";
+    $stmt = $con->prepare($sql);
+    $stmt->bindParam(':usuario', $usuario, PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $hashedPassword = $row['password'];
+        $pass_c  = sha1 ($contra);
+
+        if($hashedPassword  == $pass_c){
+            $_SESSION['nombre'] = $row['nombre'];
+            header("Location:admin/index.php");
+        }
+        // Verificar la contraseña
+        if(password_verify($contra, $hashedPassword)) {
+            // La contraseña es correcta, puedes permitir el acceso
+            echo "Contraseña válida. Acceso permitido.";
+        } else {
+            // La contraseña no coincide
+            echo "Contraseña incorrecta. Acceso denegado.";
+        }
+    } else {
+        // No se encontró el usuario en la base de datos
+        echo "Usuario no encontrado. Acceso denegado.";
+    }
+}
 ?>
 
 
@@ -68,16 +105,16 @@
         <section class="row">
             <div class="col"  data-aos="fade-up" >
                 <h4>Iniciar Sesión</h4>
-                <form >
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
 
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="text" name="usuario" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text">Sesion administrativa</div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" name="contra" class="form-control" id="exampleInputPassword1">
                     </div>
                     <button type="submit" class="btn btn-danger w-50">INGRESAR</button>
                 </form>
